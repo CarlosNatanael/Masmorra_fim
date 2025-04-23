678b907558d06953673142e009f664673b737af2
Srleo12 <carlosnatha2345@gmail.com>
2025-04-23 16:13:14 -0300

    .

--- conquistas_imag/conquista.py
+++ conquistas_imag/conquista.py
@@ -1,6 +1,6 @@
 from winotify import Notification
 toast = Notification(app_id="Masmorra do Fim",
-                     title="Conquista",
-                     msg="Descobriu o autor original da peça.",
-                     icon=r"C:\Users\SUPORTE 03\Desktop\EU\Eu (Carlos)\Cod_Dev\Masmorra_fim\conquistas_imag\gramor.png")
+                     title="Guardião da Lei",
+                     msg="Cumpra cada regra sem questionar. A ordem é sua armadura.",
+                     icon=r"C:\Users\SUPORTE 03\Desktop\EU\Eu (Carlos)\Cod_Dev\Masmorra_fim\conquistas_imag\anjo.png")
 toast.show()
