8d44100a930a3b476e37a29b65e0007e0bceaa15
Srleo12 <carlosnatha2345@gmail.com>
2025-04-25 11:51:19 -0300

    .

--- niveis/nivel6_2.py
+++ niveis/nivel6_2.py
@@ -60,8 +60,8 @@ Você pode:
             print("Sem oferecer algo, você é forçado a enfrentar os esqueletos.\n")
             time.sleep(5)
             inimigos = [
-                {"nome": "Esqueleto Guerreiro", "classe": "Guerreiro", "vida": 70, "força": 45, "defesa": 60, "nivel":7},
-                {"nome": "Esqueleto Arqueiro", "classe": "Arqueiro", "vida": 60, "força": 47, "defesa": 40, "nivel":7}
+                {"nome": "Esqueleto Guerreiro", "classe": "Guerreiro", "vida": 80, "força": 45, "defesa": 66, "nivel":7},
+                {"nome": "Esqueleto Arqueiro", "classe": "Arqueiro", "vida": 70, "força": 47, "defesa": 59, "nivel":7}
             ]
             input("Prepare-se para o combate! Pressione ENTER...\n")
             if not combate(player, inimigos):
