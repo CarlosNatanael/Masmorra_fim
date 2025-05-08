5d78a101cb4574a69d10552502733ddc576da4b1
Srleo12 <carlosnatha2345@gmail.com>
2025-05-08 12:44:04 -0300

    erro caminho

--- conquistas_imag/conquista.py
+++ conquistas_imag/conquista.py
@@ -5,35 +5,35 @@ conquistas = {
         "titulo": "Guardião da Lei",
         "descricao": "Cumpra cada regra sem questionar. A ordem é sua armadura.",
         "icone": "conquistas_imag/guardiaoLei.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "coracao_heroico": {
         "nivel": 1,
         "titulo": "Coração Heroico",
         "descricao": "Proteja os fracos, mesmo contra as leis da masmorra.",
         "icone": "conquistas_imag/coracaoHeroi.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "pacifista_iluminado": {
         "nivel": 1,
         "titulo": "Pacifista Iluminado",
         "descricao": "Resolva todos os conflitos sem derramar sangue.",
         "icone": "conquistas_imag/pacifista1.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "sombra_sorte_1": {
         "nivel": 1,
         "titulo": "Sombra da Sorte I",
         "descricao": "Passe pelo nível sem enfrentar nenhum combate.",
         "icone": "conquistas_imag/sorte.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "exterminador_goblin_1": {
         "nivel": 1,
         "titulo": "Exterminador Goblin I",
         "descricao": "Limpe a masmorra de toda a raça goblinóide.",
         "icone": "conquistas_imag/goblin.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     
     # Nível 2
@@ -42,14 +42,14 @@ conquistas = {
         "titulo": "Sombra da Sorte II",
         "descricao": "Atravesse o salão sem cruzar sua lâmina.",
         "icone": "conquistas_imag/sorte.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "encontro_eu": {
         "nivel": 2,
         "titulo": "O Encontro do Eu",
         "descricao": "Derrote seu próprio reflexo corrompido.",
         "icone": "conquistas_imag/encontroeu.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     
     # Nível 3
@@ -58,21 +58,21 @@ conquistas = {
         "titulo": "Exterminador Goblin II",
         "descricao": "Erradique a infestação goblin nas ruínas do conhecimento.",
         "icone": "conquistas_imag/goblin.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "cacador_golem_1": {
         "nivel": 3,
         "titulo": "Caçador de Golem I",
         "descricao": "Reduza o Golem de Elbano a pó e ruínas.",
         "icone": "conquistas_imag/golem.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "velho_guardiao": {
         "nivel": 3,
         "titulo": "O Velho Guardião",
         "descricao": "Supere Velkar e seus segredos arcanos.",
         "icone": "conquistas_imag/velkar.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     
     # Nível 4
@@ -81,42 +81,42 @@ conquistas = {
         "titulo": "Sombra da Sorte III",
         "descricao": "Navegue pelo pântano sem atrair atenção indesejada.",
         "icone": "conquistas_imag/sorte.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "quebrador_pedra_2": {
         "nivel": 4,
         "titulo": "Quebrador de Pedra II",
         "descricao": "Destrua o Golem do Pântano em seu próprio domínio.",
         "icone": "conquistas_imag/golem.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "devorador_devorado": {
         "nivel": 4,
         "titulo": "O Devorador Devorado",
         "descricao": "Leve Grumor a conhecer o sabor de sua própria medicina.",
         "icone": "conquistas_imag/gramor.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "quebrador_pedra_3_fogo": {
         "nivel": 4,
         "titulo": "Quebrador de Pedra III",
         "descricao": "Extinga a chama do Golem Ancestral de Fogo.",
         "icone": "conquistas_imag/golem.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "quebrador_pedra_3_terra": {
         "nivel": 4,
         "titulo": "Quebrador de Pedra III",
         "descricao": "Extinga o Golem Ancestral de Terra.",
         "icone": "conquistas_imag/golem.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "punhos_aco": {
         "nivel": 4,
         "titulo": "Punhos de Aço",
         "descricao": "Complete o nível usando apenas seus punhos como armas.",
         "icone": "conquistas_imag/punhodeaco.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     
     # Nível 5
@@ -125,28 +125,28 @@ conquistas = {
         "titulo": "O Fim do Tirano",
         "descricao": "Derrube Eldramar de seu trono de ossos.",
         "icone": "conquistas_imag/fimdotirano.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "matador_labirinto": {
         "nivel": 5,
         "titulo": "Matador do Labirinto",
         "descricao": "Leve o Minotauro ao seu último suspiro.",
         "icone": "conquistas_imag/minotaur.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "domador_dragoes": {
         "nivel": 5,
         "titulo": "Domador de Dragões",
         "descricao": "Subjugue a fera das sombras com fogo e aço.",
         "icone": "conquistas_imag/dragon.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "mestre_mentiras": {
         "nivel": 5,
         "titulo": "Mestre das Mentiras",
         "descricao": "Engane o próprio guardião da masmorra.",
         "icone": "conquistas_imag/mestredasmentiras.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     
     # Nível 6_1
@@ -155,35 +155,35 @@ conquistas = {
         "titulo": "Anjo da Perdição",
         "descricao": "Derrube o celestial corrompido de seu pedestal.",
         "icone": "conquistas_imag/anjo.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "libertador_correntes": {
         "nivel": 6,
         "titulo": "Libertador das Correntes",
         "descricao": "Libertou todos no Cárcere das Almas Perdidas.",
         "icone": "conquistas_imag/libertandocorrentes.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "carcereiro_eterno": {
         "nivel": 6,
         "titulo": "Carcereiro Eterno",
         "descricao": "Condene os prisioneiros à escuridão perpétua.",
         "icone": "conquistas_imag/carcereiro.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "juiz_sombras": {
         "nivel": 6,
         "titulo": "Juiz das Sombras",
         "descricao": "Assuma o manto do verdugo celestial.",
         "icone": "conquistas_imag/juizdassombras.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "coroa_trevas": {
         "nivel": 6,
         "titulo": "Coroa das Trevas",
         "descricao": "Ascenda como o novo Monarca das Sombras.",
         "icone": "conquistas_imag/monarca.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     
     # Nível 6_2
@@ -192,35 +192,35 @@ conquistas = {
         "titulo": "Memórias Perdidas",
         "descricao": "Pague com suas lembranças mais preciosas pela passagem.",
         "icone": "conquistas_imag/memoriasperdidas.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "danca_ossos": {
         "nivel": 6,
         "titulo": "Dança dos Ossos",
         "descricao": "Derrote a legião de esqueletos famintos.",
         "icone": "conquistas_imag/dancaesqueletos.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "banquete_wendigo": {
         "nivel": 6,
         "titulo": "O Banquete do Wendigo",
         "descricao": "Sirva-se da carne do monstro que queria devorá-lo.",
         "icone": "conquistas_imag/wendigo.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "fim_lorde": {
         "nivel": 6,
         "titulo": "O Fim do Lorde",
         "descricao": "Leve o Lorde dos Desesperados ao seu destino final.",
         "icone": "conquistas_imag/lordedesesperados.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "abraco_trevas": {
         "nivel": 6,
         "titulo": "Abraço das Trevas",
         "descricao": "Beba do poder sombrio e renasça como Monarca.",
         "icone": "conquistas_imag/monarca.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     
     # Nível 6_3
@@ -229,28 +229,28 @@ conquistas = {
         "titulo": "Estrela das Sombras",
         "descricao": "Ascenda como o novo Monarca das Sombras.",
         "icone": "conquistas_imag/monarca.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "sombra_sorte_4": {
         "nivel": 6,
         "titulo": "Sombra da Sorte IV",
         "descricao": "Complete o nível sem participar do espetáculo sangrento.",
         "icone": "conquistas_imag/sorte.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "fim_espetaculo": {
         "nivel": 6,
         "titulo": "O Fim do Espetáculo",
         "descricao": "Derrube o Diretor e rasgue seu roteiro de tormento.",
         "icone": "conquistas_imag/diretor.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
         "sombra_sorte_5": {
         "nivel": 6,
         "titulo": "Sombra da Sorte V",
         "descricao": "Avance com o bonus do diretor.",
         "icone": "conquistas_imag/sorte.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     # Nível 7_humano
     "silenciador_eco": {
@@ -258,14 +258,14 @@ conquistas = {
         "titulo": "Silenciador de Ecos",
         "descricao": "Derrotou o Colecionador de Nomes e calou a catatumbas.",
         "icone": "conquistas_imag/colecionador.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "sagrado_recipiente": {
         "nivel": 7,
         "titulo": "Sagrado Recipiente",
         "descricao": "Recebeu a benção divina Sunshine.",
         "icone": "conquistas_imag/sunshine.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     # Nível 7_sombra
     "quebrador_juramentos": {
@@ -273,14 +273,14 @@ conquistas = {
         "titulo": "Quebrador de Juramentos",
         "descricao": "Derrotou Altherion o consagrado, manchando seu manto sagrado.",
         "icone": "conquistas_imag/altherion.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "eclipse_eterno": {
         "nivel": 7,
         "titulo": "Eclipse Eterno",
         "descricao": "Como Monarca das Sombras, conjurou um eclipse que obscureceu o sol por sete dias.",
         "icone": "conquistas_imag/eclipse.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     # Nível 8
     "coração_libertado_8": {
@@ -288,28 +288,28 @@ conquistas = {
         "titulo": "Coração Libertado",
         "descricao": "Você derrotou Velthurion e quebrou a Forja das Almas.",
         "icone": "conquistas_imag/forja.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "mestre_dos_baús": {
         "nivel": 8,
         "titulo": "Mestre dos Baús",
         "descricao": "Abriu todos os baús no Coração da Masmorra sem ser devorado.",
         "icone": "conquistas_imag/mestre_baus.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "devorador_de_mimicos": {
         "nivel": 8,
         "titulo": "Devorador de Mímicos",
         "descricao": "Derrotou o Baú Mímico.",
         "icone": "conquistas_imag/devorador_mimicos.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "artesao_da_sorte": {
         "nivel": 8,
         "titulo": "Artesão da Sorte",
         "descricao": "Obteve itens lendários de um único baú.",
         "icone": "conquistas_imag/artesao_sorte.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
 
         # Nível 9
@@ -318,20 +318,20 @@ conquistas = {
         "titulo": "A Última Luz",
         "descricao": "Testemunhou o sacrifício de Valysse e sobreviveu ao ataque de Eldramar.",
         "icone": "conquistas_imag/ultima_luz.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "guardiao_perdido": {
         "nivel": 9,
         "titulo": "Guardião dos Perdidos",
         "descricao": "Visitou todos os locais do Refúgio dos Esquecidos antes do ataque.",
         "icone": "conquistas_imag/guardiao_perdido.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     },
     "furia_elemental": {
         "nivel": 9,
         "titulo": "Fúria Elemental",
         "descricao": "Recebeu o último presente de Valysse - a Pedra da Fúria Elemental.",
         "icone": "conquistas_imag/furia_elemental.png",
-        "som": "game_sound/beep3-98810.mp3"
+        "som": "sound/beep3-98810.mp3"
     }
 }
